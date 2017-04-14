1919653e0cc34f898d606fb0c0c5e97ed58ef9a3
bye221 <louis-come.auvray@hei.fr>
2017-04-14 15:57:07 +0200

    Classe library ajoutée

---
+++ src/main/java/marquise/services/InformationLibrary.java
@@ -0,0 +1,54 @@
+package marquise.services;
+import java.util.List;
+
+import marquise.daos.InformationDao;
+import marquise.daos.UtilisateurDao;
+import marquise.daos.impl.InformationDaoImpl;
+import marquise.daos.impl.UtilisateurDaoImpl;
+import marquise.projos.Information;
+import marquise.projos.Utilisateur;
+
+public class InformationLibrary {
+	
+	
+	private static class InformationLibraryHolder{
+		private final static InformationLibrary instance = new InformationLibrary();
+		
+	}
+	
+	public static InformationLibrary getInstance(){
+		return InformationLibraryHolder.instance;
+	}
+	
+	private InformationDao informationDao = new InformationDaoImpl();
+	private UtilisateurDao utilisateurDao = new UtilisateurDaoImpl();	
+
+	private InformationLibrary() {
+	}
+
+	public List<Information> listFilms() {
+		return informationDao.listInformations();
+	}
+
+	public Information getInformation(Integer id) {
+		return informationDao.getInformation(id);
+	}
+
+	public Information addInformation(Information information) {
+		return informationDao.addInformation(information);
+	}
+
+	public List<Utilisateur> listUtilisateurs() {
+		return utilisateurDao.listUtilisateurs();
+	}
+
+	public Utilisateur getUtilisateur(Integer id) {
+		return utilisateurDao.getUtilisateur(id);
+	}
+
+	public Utilisateur addUtilisateur(String nom, String prenom) {
+		return utilisateurDao.addUtilisateur(nom, prenom);
+	}
+	
+	
+}
