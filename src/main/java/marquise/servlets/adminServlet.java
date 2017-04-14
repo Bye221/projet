91d9265e6df0fee478d45590facaa24106adf847
bye221 <louis-come.auvray@hei.fr>
2017-04-14 21:02:42 +0200

    Dernier commit de la journée
    problème avec méthodes doPost

--- src/main/java/marquise/servlets/adminServlet.java
+++ src/main/java/marquise/servlets/adminServlet.java
@@ -1,6 +1,7 @@
 package marquise.servlets;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,22 +11,35 @@ import javax.servlet.http.HttpServletResponse;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import marquise.projos.Utilisateur;
 import marquise.services.InformationLibrary;
 
 @WebServlet("/admin")
 public class adminServlet extends AbstractGenericServlet {
 	
-	private static final long serialVersionUID = 1L;
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4835451077585731550L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		
 		TemplateEngine templateEngine = this.createTemplateEngine(req);
 		
 		WebContext context = new WebContext(req, resp, req.getServletContext());
+		
 		context.setVariable("utilisateurs", InformationLibrary.getInstance().listUtilisateurs());
+		
 
 		templateEngine.process("admin/admin", context, resp.getWriter());
 	}
+
+
+	
+	
 
 }
