8b5576b78da236706d7c86bf8969e7f35257468f
bye221 <louis-come.auvray@hei.fr>
2017-05-02 16:26:03 +0200

    Changement mineurs

--- src/main/java/marquise/servlets/adminBis.java
+++ src/main/java/marquise/servlets/adminBis.java
@@ -1,33 +1,33 @@
 package marquise.servlets;
 
 import java.io.IOException;
-import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import marquise.projos.Utilisateur;
 import marquise.services.InformationLibrary;
 
-@WebServlet("/admin")
-public class adminServlet extends AbstractGenericServlet {
+
+@WebServlet("/adminBis")
+public class adminBis extends AbstractGenericServlet {
 	
-	private int idRecherche;
-	private int idInformation;
+	int idRecherche;
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -4835451077585731550L;
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
 		
 		TemplateEngine templateEngine = this.createTemplateEngine(req);
 		
@@ -37,14 +37,10 @@ public class adminServlet extends AbstractGenericServlet {
 		
 		context.setVariable("utilisateurs", InformationLibrary.getInstance().listUtilisateurs());
 		
-		
+
 		if(idRecherche != 0){
 			context.setVariable("idUtils", InformationLibrary.getInstance().getUtilisateur(idRecherche));
 		}
-		//else
-		//if(idInformation != 0){
-		//	context.setVariable("idInfos", InformationLibrary.getInstance().getInformation(idInformation));
-		//}
 
 		templateEngine.process("admin/admin", context, resp.getWriter());
 	}
@@ -58,10 +54,6 @@ public class adminServlet extends AbstractGenericServlet {
 		resp.sendRedirect("admin");
 	
 	}
-	
-	
-
-
 	
 	
 
---
+++ src/main/java/marquise/servlets/coursServlet.java
@@ -0,0 +1,39 @@
+package marquise.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+
+@WebServlet("/cours")
+public class coursServlet extends AbstractGenericServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		TemplateEngine templateEngine = this.createTemplateEngine(req);
+		
+		resp.setCharacterEncoding("UTF-8");
+		
+		WebContext context = new WebContext(req, resp, req.getServletContext());
+
+		templateEngine.process("users/cours", context, resp.getWriter());
+	}
+	
+	
+
+}
